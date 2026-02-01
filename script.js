0f49e1989901c44eac39f4eae987454bd33b0d60
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2026-02-01 13:49:41 +0000

    add build version footer and 404 page

---
+++ script.js
@@ -0,0 +1,12 @@
+const user = 'angrypig555';
+const repo = 'brny-dev';
+
+fetch(`https://api.github.com/repos/${user}/${repo}/commits/main`)
+  .then(response => response.json())
+  .then(data => {
+    const sha = data.sha.substring(0, 7); // Shorten the ID
+    const link = document.getElementById('gh-link');
+    link.innerText = sha;
+    link.href = `https://github.com/${user}/${repo}/commit/${data.sha}`;
+  })
+  .catch(err => console.error('Could not fetch version', err));
